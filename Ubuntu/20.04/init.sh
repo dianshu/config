5ec33ea34cf991192f730bee2aa7b15f506ce492
dianshu <feishicheng2015@163.com>
2022-07-25 10:02:07 +0800

    Update init.sh

--- Ubuntu/20.04/init.sh
+++ Ubuntu/20.04/init.sh
@@ -1,36 +1,38 @@
+#! /bin/bash
+export username=${SUDO_USER:-`whoami`}
+
 # add sudo right to current user
 mkdir -p /etc/sudoers.d/
-echo "`whoami`  ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/`whoami`
+echo "${username}  ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/${username}
 
 # upgrade packages
 apt update && apt upgrade -y
 
 # timezone related work
-export DEBIAN_FRONTEND=noninteractive
 export TZ=Asia/Shanghai
-apt install -y tzdata
+DEBIAN_FRONTEND=noninteractive apt install -y tzdata
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 # zsh related work
 apt install -y zsh
-curl https://raw.githubusercontent.com/dianshu/config/master/zsh/.zshrc > ~/.zshrc
-chsh -s `which zsh`
+curl https://raw.githubusercontent.com/dianshu/config/master/zsh/.zshrc > /home/${username}/.zshrc
+chsh -s `which zsh` ${username}
 
-rm -rf ~/.zsh/plugins/zsh-abbr
-git clone --depth 1 https://github.com/olets/zsh-abbr ~/.zsh/plugins/zsh-abbr
+rm -rf /home/${username}/.zsh/plugins/zsh-abbr
+git clone --depth 1 https://github.com/olets/zsh-abbr /home/${username}/.zsh/plugins/zsh-abbr
 
-rm -rf ~/.zsh/plugins/zsh-autosuggestions
-git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions
+rm -rf /home/${username}/.zsh/plugins/zsh-autosuggestions
+git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions /home/${username}/.zsh/plugins/zsh-autosuggestions
 
-rm -rf ~/.zsh/plugins/zsh-syntax-highlighting
-git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/plugins/zsh-syntax-highlighting
+rm -rf /home/${username}/.zsh/plugins/zsh-syntax-highlighting
+git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting /home/${username}/.zsh/plugins/zsh-syntax-highlighting
 
-rm -rf ~/.zsh/plugins/zsh-history-substring-search
-git clone --depth 1 https://github.com/zsh-users/zsh-history-substring-search ~/.zsh/plugins/zsh-history-substring-search
+rm -rf /home/${username}/.zsh/plugins/zsh-history-substring-search
+git clone --depth 1 https://github.com/zsh-users/zsh-history-substring-search /home/${username}/.zsh/plugins/zsh-history-substring-search
 
 # install homebrew
 apt install -y build-essential procps curl file git
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+NONINTERACTIVE=1 su - ${username} -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew doctor
 
