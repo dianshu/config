769e92bb35d62ee7f7c2ae6ab12a4fb31a68b6ed
dianshu <feishicheng2015@163.com>
2022-08-01 14:34:39 +0800

    Update user-specific.sh

--- Ubuntu/20.04/user-specific.sh
+++ Ubuntu/20.04/user-specific.sh
@@ -27,7 +27,7 @@ rm -rf ~/.zsh/plugins/zsh-history-substring-search
 git clone --depth 1 https://github.com/zsh-users/zsh-history-substring-search ~/.zsh/plugins/zsh-history-substring-search
 
 command -v zsh | sudo tee -a /etc/shells
-sudo chsh -s `command -v zsh` ${UserName}
+sudo chsh -s `command -v zsh` ${USER}
 
 # vim related work
 cat > ~/.vimrc << EOF
