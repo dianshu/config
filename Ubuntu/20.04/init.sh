065f3ade19ee925d88b28a94237bc3e2ed7a43dc
dianshu <feishicheng2015@163.com>
2022-08-21 18:55:55 +0800

    Update init.sh

--- Ubuntu/20.04/init.sh
+++ Ubuntu/20.04/init.sh
@@ -22,7 +22,29 @@ DEBIAN_FRONTEND=noninteractive apt install -y tzdata
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 # install basic packages
-apt install -y build-essential procps curl file git
+apt install -y build-essential procps curl file git software-properties-common apt-transport-https wget
+
+# vscode related work
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
+add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+apt install -y code
+
+# install vscode extensions
+code \
+    --install-extension adpyke.codesnap \
+    --install-extension eamodio.gitlens \
+    --install-extension hediet.vscode-drawio \
+    --install-extension ms-azure-devops.azure-pipelines \
+    --install-extension ms-dotnettools.csharp \
+    --install-extension ms-dotnettools.vscode-dotnet-runtime \
+    --install-extension ms-python.python \
+    --install-extension ms-python.vscode-pylance \
+    --install-extension ms-toolsai.jupyter \
+    --install-extension ms-toolsai.jupyter-keymap \
+    --install-extension ms-toolsai.jupyter-renderers \
+    --install-extension ms-vscode-remote.remote-wsl \
+    --install-extension ms-vscode.azure-account \
+    --install-extension redhat.vscode-yaml
 
 # git
 git config --global alias.l "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
