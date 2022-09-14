e51027713320813906274ca694719398cb7809a5
dianshu <feishicheng2015@163.com>
2022-09-14 12:06:52 +0800

    Update init.sh

--- Ubuntu/20.04/init.sh
+++ Ubuntu/20.04/init.sh
@@ -24,4 +24,4 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 # install basic packages
 apt install -y build-essential procps curl file git software-properties-common apt-transport-https wget
 
-sudo -u ${UserName} /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/dianshu/config/HEAD/Ubuntu/20.04/user-specific.sh?${RANDOM})" ${GitUserName} ${GitUserEmail}
+sudo -u ${UserName} /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/dianshu/config/HEAD/Ubuntu/20.04/user-specific.sh?${RANDOM})" ${GitUserName} ${GitUserEmail} ${UserName}
