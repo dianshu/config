f7c93dcbc0291967376d08047c974798684fd8b9
dianshu <feishicheng2015@163.com>
2023-02-27 09:53:50 +0800

    Update for-dev.sh

--- Ubuntu/20.04/for-dev.sh
+++ Ubuntu/20.04/for-dev.sh
@@ -52,6 +52,10 @@ set foldenable
 set foldlevelstart=10
 set foldmethod=indent
 set backspace=indent,eol,start
+
+set cursorcolumn
+set cursorline
+highlight CursorLine   cterm=NONE ctermbg=black ctermfg=yellow
 EOF
 
 # python related work
