8b86408d0dabc3939295b0a6fbca5af2ed01ea35
dianshu <27887844+dianshu@users.noreply.github.com>
2024-12-27 16:22:15 +0800

    Update user-specific.sh

--- Ubuntu/24.04/user-specific.sh
+++ Ubuntu/24.04/user-specific.sh
@@ -86,13 +86,13 @@ if uname -a | grep -qi "WSL"; then
     sudo wget https://raw.githubusercontent.com/dianshu/config/master/Ubuntu/24.04/wsl.conf -O /etc/wsl.conf
     
     # install docker
-    sudo apt install -y ca-certificates curl gnupg lsb-release
-    sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt update
-    sudo apt install -y docker-ce docker-ce-cli containerd.io
-    sudo usermod -aG docker $2
+    # sudo apt install -y ca-certificates curl gnupg lsb-release
+    # sudo mkdir -p /etc/apt/keyrings
+    # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    # echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    # sudo apt update
+    # sudo apt install -y docker-ce docker-ce-cli containerd.io
+    # sudo usermod -aG docker $2
 
     # use browser in windows
     sudo apt install -y wslu
