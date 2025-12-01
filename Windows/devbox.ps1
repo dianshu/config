02a242c63fcb9e172e357c41862e918ad453d327
dianshu <27887844+dianshu@users.noreply.github.com>
2025-12-01 11:18:20 +0800

    Add Windows Terminal settings overwrite functionality
    Added functionality to overwrite Windows Terminal settings.json from a remote URL.

--- Windows/devbox.ps1
+++ Windows/devbox.ps1
@@ -84,6 +84,11 @@ foreach ($extension in $vscodeExtensions) {
 # Overwrite pwsh profile
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dianshu/config/refs/heads/master/Windows/pwsh_profile.ps1" | Select-Object -ExpandProperty Content | Set-Content -Path $PROFILE -Force
 
+# Overwrite windows terminal settings.json
+$remoteFIle = "https://raw.githubusercontent.com/dianshu/config/refs/heads/master/Windows/windows_terminal.json"
+$localPath = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+Invoke-WebRequest -Uri $remoteUrl -OutFile $localPath -Force
+
 wsl --update
 wsl --install --no-launch Ubuntu-24.04
 Write-Output 'Init script for Ubuntu-24.04: sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/dianshu/config/HEAD/Ubuntu/24.04/init.sh?${RANDOM})"'
