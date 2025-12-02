c54b8b35814794255e8f2d019019198292dc9d36
dianshu <27887844+dianshu@users.noreply.github.com>
2025-12-02 16:46:58 +0800

    Fix formatting and add newline in pwsh_profile.ps1

--- Windows/pwsh_profile.ps1
+++ Windows/pwsh_profile.ps1
@@ -2,7 +2,7 @@ function Prompt {
     $currentTime = Get-Date -Format "HH:mm:ss"
     Write-Host "$currentTime" -NoNewline -ForegroundColor Green
     Write-Host " | " -NoNewline -ForegroundColor Gray
-    
+
     # Check if current directory is inside a Git repository
     if (git status) {
         $branch = git rev-parse --abbrev-ref HEAD 2>$null
