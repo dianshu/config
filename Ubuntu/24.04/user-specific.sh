cf5a2aba76dbed7e55bf90e0e906674a4d1dd2f5
dianshu <27887844+dianshu@users.noreply.github.com>
2026-02-23 09:47:36 +0800

    Initialize commands and skills in user-specific.sh
    Added commands and skills initialization in user-specific.sh

--- Ubuntu/24.04/user-specific.sh
+++ Ubuntu/24.04/user-specific.sh
@@ -38,8 +38,12 @@ curl -fsSL https://claude.ai/install.sh | bash
 mkdir -p $HOME/.claude
 wget https://raw.githubusercontent.com/dianshu/config/master/claude/settings.json -O $HOME/.claude/settings.json
 wget https://raw.githubusercontent.com/dianshu/config/master/claude/statusline.sh -O $HOME/.claude/statusline.sh
+
+# Init commands
 mkdir -p $HOME/.claude/commands
 wget https://raw.githubusercontent.com/dianshu/config/master/claude/commands/fix-vulns.md -O $HOME/.claude/commands/fix-vulns.md
+
+# Init skills
 mkdir -p $HOME/.claude/skills/find-skills
 wget https://raw.githubusercontent.com/openstatusHQ/openstatus/refs/heads/main/.agents/skills/find-skills/SKILL.md -O $HOME/.claude/skills/find-skills/SKILL.md
 
