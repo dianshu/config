86c520edb8b252187d74355022fc08a137bd67e0
feiyuegeng <feiyuegeng@microsoft.com>
2026-04-01 13:42:58 +0000

    Add ruff to brew install packages

--- Ubuntu/24.04/user-specific.sh
+++ Ubuntu/24.04/user-specific.sh
@@ -15,7 +15,7 @@ ulimit -n 100000
 # install packages
 brew install azure-cli yq
 brew install jq net-tools git tree tmux
-brew install trivy uv node
+brew install trivy uv node ruff
 brew install gh frpc
 curl -fsSL https://bun.sh/install | bash
 
